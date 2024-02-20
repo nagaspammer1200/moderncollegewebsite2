698762b0adcac93d2bfc15114868e4b9ededdfad
pssreekanth778@gmail.com <pssreekanth778@gmail.com>
2024-02-20 12:40:49 +0530

    secsd

--- src/pages/PDean.jsx
+++ src/pages/PDean.jsx
@@ -14,7 +14,7 @@ const PDean = () => {
           <div className="mb-4"><PermanentNavbar/></div>
           <div><FixedNavbar/></div>
           <div><PersonDetails personimage="src/images/people/itdean.jpg" personname="Dr. Vijayanand K. S." persondepartment="Professor, Dept. of Information Technology" personnumber="+91-&nbsp;9447865892" personemail="ksvijayanand@gmail.com" persondesignation="Professor, Department of Information Technology, Government Engineering College, Bartonhill, Trivandrum." personphd="– PhD in Security in Cloud Computing, Anna University"
-          personmtech="– M.Tech ,Computer Science, Anna University " personbtech="– B.Tech IT , CUSAT" personresearch=", Research Area: Cloud Computing" personprofile="http://gecbh.ac.in/media/docs/Faculty_Profile-Dr.Vijayanand_K_S-with_foto.pdf" /></div>
+          personmtech="– M.Tech ,Computer Science,   Anna University " personbtech="– B.Tech IT , CUSAT" personresearch=", Research Area: Cloud Computing" personprofile="http://gecbh.ac.in/media/docs/Faculty_Profile-Dr.Vijayanand_K_S-with_foto.pdf" /></div>
           <div><BannerFooter/></div>
           <div><PermanentFooter/></div>
         </div>
