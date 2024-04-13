5c117b8fae3fe16173cb3c717ef355c15efb2e00
pssreekanth778@gmail.com <pssreekanth778@gmail.com>
2024-04-13 12:27:17 +0530

    Final Push2

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
