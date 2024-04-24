35d1bd4027d0b7f7ffc61b459b2ced8c5d15b148
pssreekanth778@gmail.com <pssreekanth778@gmail.com>
2024-04-25 04:35:27 +0530

    Final Push 21

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,3 +1,4 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBt59b_Mt5T49xGcufVUn5-78dAAbHrXgs",
   authDomain: "gecbhclone.firebaseapp.com",
   projectId: "gecbhclone",
-  storageBucket: "gs://gecbhclone.appspot.com",
+  storageBucket: "gecbhclone.appspot.com",
   messagingSenderId: "416594936431",
   appId: "1:416594936431:web:dfe9f3f70d232ce72ec39a"
 };
