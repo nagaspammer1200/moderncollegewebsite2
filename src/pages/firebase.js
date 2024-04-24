aa3cdddbff8261b94674742cf2b56ba8d5f9d0da
pssreekanth778@gmail.com <pssreekanth778@gmail.com>
2024-04-25 04:32:50 +0530

    Final Push 20

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBt59b_Mt5T49xGcufVUn5-78dAAbHrXgs",
   authDomain: "gecbhclone.firebaseapp.com",
   projectId: "gecbhclone",
-  storageBucket: "gecbhclone.appspot.com",
+  storageBucket: "gs://gecbhclone.appspot.com",
   messagingSenderId: "416594936431",
   appId: "1:416594936431:web:dfe9f3f70d232ce72ec39a"
 };
