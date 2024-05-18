293cf12f2a8329de75a0a4588217f688563f53a2
pssreekanth778@gmail.com <pssreekanth778@gmail.com>
2024-05-18 14:03:44 +0530

    Fixing Width of Floating Text Component

--- src/components/FloatingText.jsx
+++ src/components/FloatingText.jsx
@@ -12,12 +12,14 @@ const FloatingText = () => {
 
 
   return (
-    <div className={darkMode?"bg-dark text-warning border-top border-2 border-primary":"bg-warning text-black border-top border-2 border-success"}>
+    <div className={darkMode?"bg-dark floatingtextcontainerbox text-warning border-top border-2 border-primary":"bg-warning text-black floatingtextcontainerbox border-top border-2 border-success"}>
         <div className="floatingtextcontainer">
             <div className="floatingtextcontaineritem"><h4><b>FLASH NEWS:</b> </h4></div>
             <div className="floatingtextcontaineritem"><p><b>Online applications for New Adam Batch</b></p></div>
             <div className="floatingtextcontaineritem"><p><b>First Year Student Induction Program 2023</b></p></div>
             <div className="floatingtextcontaineritem"><p><b>M. Tech Admissions 2023</b></p></div>
+            <div className="floatingtextcontaineritem"><p><b>Summer Vacation In June</b></p></div>
+            <div className="floatingtextcontaineritem"><p><b>University Exams Start in July!</b></p></div>
         </div>
     </div>
   )
